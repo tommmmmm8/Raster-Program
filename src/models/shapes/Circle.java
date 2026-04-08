e7e16db905fc89f3330fe55d4cee12c656353d81
Tom <tom8.chr@gmail.com>
2026-04-08 21:38:00 +0200

    Refactor Circle class to extend BaseShape and update constructor and drawing logic with color and style parameters

--- src/models/shapes/Circle.java
+++ src/models/shapes/Circle.java
@@ -1,20 +1,17 @@
-package models;
+package models.shapes;
 
 import rasterizers.Rasterizer;
 import rasters.Raster;
 
 import java.awt.*;
 
-public class Circle implements Shape {
-
+public class Circle extends BaseShape {
     private Point center;
     private int radius;
-    private boolean isDotted;
 
-    public Circle(Point center, Point edge, boolean isDotted) {
+    public Circle(Point center, Point edge, Color color, Style style, int width)  {
+        super(color, style, width);
         this.center = center;
-        this.isDotted = isDotted;
-
         int dx = center.getX() - edge.getX();
         int dy = center.getY() - edge.getY();
         this.radius = (int) Math.sqrt(dx * dx + dy * dy);
@@ -22,17 +19,29 @@ public class Circle implements Shape {
 
     @Override
     public void draw(Rasterizer rasterizer) {
+        prepareRasterizer(rasterizer);
+        
+        int halfWidth = (width - 1) / 2;
+        for (int w = -halfWidth; w <= width / 2; w++) {
+            drawBresenhamCircle(rasterizer, radius + w);
+        }
+    }
+
+    private void drawBresenhamCircle(Rasterizer rasterizer, int r) {
         int increment = 1;
-        if (isDotted)
+        if (style == Style.DOTTED)
             increment = 6;
 
+        int pixelCount = 0;
+
         int x = 0;
-        int y = radius;
-        int d = 3 - (2 * radius);
+        int y = r;
+        int d = 3 - (2 * r);
 
         while (x <= y) {
             if (x % increment == 0)
-                drawCircle(rasterizer, center.getX(), center.getY(), x, y);
+                if (style != Style.DASHED || pixelCount % 20 < 10)
+                    drawCircle(rasterizer, center.getX(), center.getY(), x, y);
 
             if (d < 0)
                 d = d + (4 * x) + 6;
@@ -41,13 +50,14 @@ public class Circle implements Shape {
                 y--;
             }
 
+            pixelCount++;
             x++;
         }
     }
 
     private void drawCircle(Rasterizer rasterizer, int cX, int cY , int x, int y) {
         Raster raster = rasterizer.getRaster();
-        int color = rasterizer.getDefaultColor().hashCode();
+        int color = rasterizer.getDefaultColor().getRGB();
 
         raster.setPixel(cX + x, cY + y, color);
         raster.setPixel(cX + y, cY + x, color);
