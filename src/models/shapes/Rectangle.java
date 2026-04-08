381782ffeb64db142915426bfd984e54646b5c53
Tom <tom8.chr@gmail.com>
2026-04-08 21:37:13 +0200

    Refactor Rectangle class to extend BaseShape and update constructor and drawing logic with color and style parameters

--- src/models/shapes/Rectangle.java
+++ src/models/shapes/Rectangle.java
@@ -1,29 +1,27 @@
-package models;
+package models.shapes;
 
 import rasterizers.Rasterizer;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rectangle implements Shape {
-
+public class Rectangle extends BaseShape {
     private Point p1, p2; // Upper left corner; Bottom right corner
-    private boolean isDotted;
 
-    public Rectangle(Point p1, Point p2) {
+    public Rectangle(Point p1, Point p2, Color color, Style style, int width) {
+        super(color, style, width);
         this.p1 = p1;
         this.p2 = p2;
-        this.isDotted = false;
     }
 
-    public Rectangle(Point p1, Point p2, boolean isDotted) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.isDotted = isDotted;
+    public Rectangle(Point p1, Point p2, Color color) {
+        this(p1, p2, color, Style.NORMAL, 1);
     }
 
     @Override
     public void draw(Rasterizer rasterizer) {
+        Color defaultColor = rasterizer.getDefaultColor();
 
         // p1.x,p1.y -> p2.x,p1.y
         // p1.x,p1.y -> p1.x,p2.y
@@ -42,12 +40,11 @@ public class Rectangle implements Shape {
 //        Point BR = p1.getX() > p2.getX() ? new Point(p1.getX(), )
 //        Point BL;
 
-
         List<Line> rectangleLines = new ArrayList<>();
-        rectangleLines.add(new Line(p1, new Point(p2.getX(), p1.getY()), isDotted));
-        rectangleLines.add(new Line(p1, new Point(p1.getX(), p2.getY()), isDotted));
-        rectangleLines.add(new Line(new Point(p1.getX(), p2.getY()), p2, isDotted));
-        rectangleLines.add(new Line(new Point(p2.getX(), p1.getY()), p2, isDotted));
+        rectangleLines.add(new Line(p1, new Point(p2.getX(), p1.getY()), color, style, width));
+        rectangleLines.add(new Line(p1, new Point(p1.getX(), p2.getY()), color, style, width));
+        rectangleLines.add(new Line(new Point(p1.getX(), p2.getY()), p2, color, style, width));
+        rectangleLines.add(new Line(new Point(p2.getX(), p1.getY()), p2, color, style, width));
 
         for (Line line : rectangleLines)
             line.draw(rasterizer);
